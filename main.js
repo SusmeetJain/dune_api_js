630c4db406771fd6e31719336f2ba875c6b3d3dc
SusmeetJain <susmeetjain01@gmail.com>
2022-09-12 12:39:16 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import { Headers } from 'node-fetch';
+import fetch from 'node-fetch';
+
+// Add the API key to an header object
+const meta = {
+    "x-dune-api-key": "0PiRuJ5J5tLj6YDJDBalzM410fsFS18I"
+};
+const header = new Headers(meta);
+
+//  Call the Dune API
+const response = await fetch('https://api.dune.com/api/v1/query/1258228/execute', {
+    method: 'POST',
+    headers: header
+});
+const body = await response.text();
+
+console.log(body);
